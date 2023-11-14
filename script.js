beea4fb9be5bb8740833f4acc7a1a6b117a27ac1
Nitin1103 <redmi1smail@gmail.com>
2023-11-15 00:38:02 +0530

    added search functionality by superhero name

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // my access token : 6601778893275317
+// https://superheroapi.com/api.php/6601778893275317/search/name
 
-const getSuperHeroImg = (id) => {
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const randBtn = document.getElementById('random');
+const searchBtn = document.querySelector('#search-btn');
+const textInput = document.querySelector('#search-input');
+
+let initialId = 1;
+
+// when page loads 
+document.addEventListener("DOMContentLoaded", function () {
+    getSuperHeroById(1);
+});
+
+/// getting superhero by id
+const getSuperHeroById = (id) => {
     const superheroid = id;
     fetch(`https://superheroapi.com/api.php/6601778893275317/${superheroid}`)
         .then(response => response.json())
@@ -14,32 +29,55 @@ const getSuperHeroImg = (id) => {
         });
 }
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    getSuperHeroImg(1);
-});
-
-// adding next and previous functionality
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const randBtn = document.getElementById('random');
-
-let initialId = 1;
+// getting super hero by name
+const getSuperHeroByName = (name) => {
+    const superheroname = name;
+    fetch(`https://superheroapi.com/api.php/6601778893275317/search/${superheroname}`)
+        .then(response => response.json())
+        .then(json => {
+            if (json.results && json.results.length > 0) {
+                const superhero = json.results[0]; // Assuming you want the first superhero found with the given name
+                initialId = superhero.id; // Update initialId with the found superhero's ID
+                document.querySelector('.picture').innerHTML = `<img src="${superhero.image.url}"/>`;
+                document.getElementById('heroId').innerHTML = 'Hero ID: ' + superhero.id;
+                document.getElementById('heroName').innerHTML = 'Name: ' + superhero.name;
+                document.getElementById('heroGender').innerHTML = 'Gender: ' + superhero.appearance.gender;
+                document.getElementById('heroHeight').innerHTML = 'Height: ' + superhero.appearance.height[0];
+                document.getElementById('heroWeight').innerHTML = 'Weight: ' + superhero.appearance.weight[1];
+            } else {
+                // Handle case when no superhero with the given name is found
+                alert('Superhero not found');
+                getSuperHeroById(1);   //  show 1st superhero as default
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching superhero by name:', error);
+        });
+}
 
 // search functionality
 
-const searchBtn = document.querySelector('#search-btn');
-const textInput = document.querySelector('#search-input');
-
 searchBtn.addEventListener('click', searchHero = () => {
     const query = textInput.value;
-    document.getElementById('heroId').innerHTML = 'Hero ID: ';
-    document.getElementById('heroName').innerHTML = 'Name: ';
-    document.getElementById('heroGender').innerHTML = 'Gender: ';
-    document.getElementById('heroHeight').innerHTML = 'Height: ';
-    document.getElementById('heroWeight').innerHTML = 'Weight: ';
-    initialId = query;
-    getSuperHeroImg(query);
+    if (!isNaN(query)) {
+        document.getElementById('heroId').innerHTML = 'Hero ID: ';
+        document.getElementById('heroName').innerHTML = 'Name: ';
+        document.getElementById('heroGender').innerHTML = 'Gender: ';
+        document.getElementById('heroHeight').innerHTML = 'Height: ';
+        document.getElementById('heroWeight').innerHTML = 'Weight: ';
+        initialId = query;
+        textInput.value = "";
+        getSuperHeroById(query);
+    } else {
+        document.getElementById('heroId').innerHTML = 'Hero ID: ';
+        document.getElementById('heroName').innerHTML = 'Name: ';
+        document.getElementById('heroGender').innerHTML = 'Gender: ';
+        document.getElementById('heroHeight').innerHTML = 'Height: ';
+        document.getElementById('heroWeight').innerHTML = 'Weight: ';
+        textInput.value = "";
+        getSuperHeroByName(query);
+
+    }
 });
 
 // next button
@@ -52,7 +90,7 @@ nextBtn.addEventListener('click', function () {
     document.getElementById('heroWeight').innerHTML = 'Weight: ';
 
     // console.log(initialId);
-    getSuperHeroImg(initialId);
+    getSuperHeroById(initialId);
     prevBtn.disabled = false;
 });
 
@@ -68,7 +106,7 @@ prevBtn.addEventListener('click', function () {
         document.getElementById('heroHeight').innerHTML = 'Height: ';
         document.getElementById('heroWeight').innerHTML = 'Weight: ';
 
-        getSuperHeroImg(initialId);
+        getSuperHeroById(initialId);
     }
     if (initialId <= 1) {
         prevBtn.disabled = true;
@@ -88,5 +126,5 @@ randBtn.addEventListener('click', function () {
     document.getElementById('heroHeight').innerHTML = 'Height: ';
     document.getElementById('heroWeight').innerHTML = 'Weight: ';
 
-    getSuperHeroImg(initialId);
+    getSuperHeroById(initialId);
 });
